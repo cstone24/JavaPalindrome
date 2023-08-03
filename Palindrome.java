230e53d032ab65bebc5947bcc75b961fe26fd26d
cstone24 <97609348+cstone24@users.noreply.github.com>
2023-08-03 10:52:18 -0400

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -12,7 +12,7 @@ public class Palindrome {
 		return reversed.toUpperCase();
 	}
 	
-	public static void isPalindrome(String input, String reversed) {
+	public void isPalindrome(String input, String reversed) {
 		String isPalindrome = input.equals(reversed) ? input + " is a palindrome." : input + " is not a palindrome";
 		System.out.println(isPalindrome);
 	}
