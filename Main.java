5c47e7cdc826bb787cf67c1f4960385ff2fa4f60
cstone24 <97609348+cstone24@users.noreply.github.com>
2023-08-03 10:54:21 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,8 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		String input = ReadInput.getUserInput();
 		String reversed = Palindrome.reverseString(input);
-		
-		var palindrome = new Palindrome();
-		palindrome.isPalindrome(input, reversed);
+		Palindrome.isPalindrome(input, reversed);
 	}
 }
